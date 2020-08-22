e015bc1e09b4c0feee31a3031e290248fec3d9d1
Sam Schott <ss2151@cam.ac.uk>
2020-08-22 19:02:40 +0200

    bumped required driver to v0.2.4

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     },
     install_requires=[
         'pyvisa',
-        'mercuryitc>=0.2.1',
+        'mercuryitc>=0.2.4',
         'numpy',
         'pyqtgraph>=0.11.0',
         'PyQt5',
