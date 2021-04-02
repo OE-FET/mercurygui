217969162aa19598bdc6db48a9420063f3a0e99f
Sam Schott <ss2151@cam.ac.uk>
2021-04-02 12:26:10 +0100

    require driver >= 0.3.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     },
     install_requires=[
         "pyvisa",
-        "mercuryitc>=0.2.4",
+        "mercuryitc>=0.3.0",
         "numpy",
         "pyqtgraph>=0.11.0",
         "PyQt5",
